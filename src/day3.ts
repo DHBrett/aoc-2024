810a5cf9ec041c2956141b48351f0dbf612abd02
David Brett <dhb@davids-mbp.mynetworksettings.com>
2024-12-06 10:41:05 -0500

    Finished day 3

--- src/day3.ts
+++ src/day3.ts
@@ -3,13 +3,22 @@ import { readFileSync } from "fs-extra";
 // parse the input
 const rawInput = readFileSync("./data/day3.txt").toString();
 
-let re = /mul\((\d{1,3}),(\d{1,3})\)/gm;
+let re = /do(n't)?\(\)|mul\((\d{1,3}),(\d{1,3})\)/gm;
 const matches = rawInput.matchAll(re);
 let mulsum = 0;
+let adding = true;
+
 for (let match of matches) {
-  let num1 = parseInt(match[1]);
-  let num2 = parseInt(match[2]);
-  mulsum += num1 * num2;
+  if (match[0] === "do()") {
+    adding = true;
+  } else if (match[0] === "don't()") {
+    adding = false;
+  } else if (adding) {
+    let num1 = parseInt(match[2]);
+    let num2 = parseInt(match[3]);
+    // console.log({match, num1, num2})
+    mulsum += num1 * num2;
+  }
 }
 
 console.log({ mulsum });
