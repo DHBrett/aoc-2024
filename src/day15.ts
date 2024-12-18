e21411d4008e24f3cb24133e14b22416e37dc948
David Brett <dhb@Davids-MacBook-Pro.local>
2024-12-18 17:25:14 -0500

    Finished with day 15

--- src/day15.ts
+++ src/day15.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs-extra";
 import { Position } from "./day6";
 
-let debug = true;
+let debug = false;
 
 let realData = "./data/day15.txt";
 let testData = "./data/day15-test.txt";
@@ -19,6 +19,7 @@ enum CellType {
 }
 
 const printBoard = (grid: CellType[][]) => {
+  console.log("\n");
   grid.forEach((row, rowNum) => {
     console.log(
       row
@@ -42,7 +43,7 @@ const printBoard = (grid: CellType[][]) => {
   });
 };
 
-const grid: CellType[][] = [];
+let grid: CellType[][] = [];
 const robot: Position = { row: 0, col: 0 };
 
 // parse the board
@@ -98,64 +99,185 @@ const inBounds = ({ row, col }: Position) => {
   return true;
 };
 
+const cloneGrid = (grid: CellType[][]) => {
+  return grid.map((row) => {
+    return [...row];
+  });
+};
+
+let moveNumber = 0;
 for (let move of moves) {
-  const nRow = robot.row + move[0];
-  const nCol = robot.col + move[1];
+  // attempt to move everything on a new grid
+  console.log({ move: moveNumber++ });
+  const newGrid = cloneGrid(grid);
+  let isValid = true;
+  const previousRobot = { ...robot };
 
-  if (!inBounds({ row: nRow, col: nCol })) {
-    continue;
-  }
-
-  // find the next cell
-  const nextCell = grid[nRow][nCol];
-  if (nextCell === CellType.Empty) {
-    // easy case, next cell is empty so just move the robot
-    robot.row = nRow;
-    robot.col = nCol;
-  } else if (nextCell === CellType.Wall) {
-    // can't advance at all
-    continue;
-  } else {
-    // we have hit a box - harder case, need to find the next empty space and swap if possible
-    let nNRow = nRow + move[0];
-    let nNCol = nCol + move[1];
+  if (Math.abs(move[1]) === 1) {
+    // we are moving horizontally
+    let needToPlace: CellType | "Robot" = "Robot";
+    let nRow = robot.row + move[0];
+    let nCol = robot.col + move[1];
     while (true) {
-      if (!inBounds({ row: nNRow, col: nNCol })) {
-        // no spaces to move to, give up
+      if (!inBounds({ row: nRow, col: nCol })) {
+        isValid = false;
         break;
       }
 
-      const nextNextCell = grid[nNRow][nNCol];
-      if (nextNextCell === CellType.Wall) {
-        // no spaces to move to, give up
+      const nextCell = newGrid[nRow][nCol];
+      if (nextCell === CellType.Wall) {
+        isValid = false;
         break;
-      // } else if (nextNextCell === CellType.Box) {
-      //   // just keep going, this box will be pushed if we find a space
-      //   // advance to the next one
-      //   nNRow = nNRow + move[0];
-      //   nNCol = nNCol + move[1];
-      } else {
-        // we have found an empty space
-        // swap the first box we hit with this space
+      }
+
+      if (nextCell === CellType.Empty) {
+        // we're done
+        if (needToPlace === "Robot") {
+          robot.col = nCol;
+          robot.row = nRow;
+        } else {
+          newGrid[nRow][nCol] = needToPlace;
+        }
+
+        break;
+      }
+
+      // current cell is something else that needs to be moved
+      let temp = needToPlace;
+      needToPlace = nextCell;
+      if (temp === "Robot") {
+        newGrid[nRow][nCol] = CellType.Empty;
         robot.row = nRow;
         robot.col = nCol;
-        grid[nRow][nCol] = CellType.Empty;
-        // grid[nNRow][nNCol] = CellType.Box;
+      } else {
+        newGrid[nRow][nCol] = temp;
+      }
+      nRow += move[0];
+      nCol += move[1];
+    }
+  } else {
+    // we are moving vertically
+    let nRow = robot.row + move[0];
+    let nCol = robot.col + move[1];
+    if (!inBounds({ row: nRow, col: nCol })) {
+      isValid = false;
+      debug && printBoard(grid);
+      continue;
+    }
+
+    const nextCell = newGrid[nRow][nCol];
+    if (nextCell === CellType.Wall) {
+      isValid = false;
+      debug && printBoard(grid);
+      continue;
+    }
+
+    if (nextCell === CellType.Empty) {
+      // We can just move the robot without any other fuss
+      robot.row = nRow;
+      robot.col = nCol;
+      debug && printBoard(grid);
+      continue;
+    }
+
+    // we need to move a box
+    let boxesToMove: {
+      leftBoxPosition: Position;
+    }[] = [];
+
+    const addBoxToList = (
+      type: CellType.LeftBox | CellType.RightBox,
+      position: Position
+    ) => {
+      const leftBoxPosition =
+        type === CellType.LeftBox
+          ? position
+          : { ...position, col: position.col - 1 };
+
+      if (
+        // not already in the list
+        !boxesToMove.find(
+          (cb) =>
+            cb.leftBoxPosition.col === leftBoxPosition.col &&
+            cb.leftBoxPosition.row === leftBoxPosition.row
+        )
+      ) {
+        boxesToMove.unshift({ leftBoxPosition });
+      }
+    };
+
+    addBoxToList(nextCell, { row: nRow, col: nCol });
+    while (boxesToMove.length) {
+      let nextBox = boxesToMove.pop();
+      let newBoxRow = nextBox.leftBoxPosition.row + move[0];
+      let newBoxLeftCol = nextBox.leftBoxPosition.col + move[1];
+      let newBoxRightCol = nextBox.leftBoxPosition.col + move[1] + 1;
+
+      if (
+        !inBounds(nextBox.leftBoxPosition) ||
+        !inBounds({ row: newBoxRow, col: newBoxRightCol }) ||
+        newGrid[newBoxRow][newBoxLeftCol] === CellType.Wall ||
+        newGrid[newBoxRow][newBoxRightCol] === CellType.Wall
+      ) {
+        isValid = false;
         break;
       }
+
+      if (
+        newGrid[newBoxRow][newBoxLeftCol] === CellType.Empty &&
+        newGrid[newBoxRow][newBoxRightCol] === CellType.Empty
+      ) {
+        // move the box here
+        newGrid[nextBox.leftBoxPosition.row][nextBox.leftBoxPosition.col] =
+          CellType.Empty;
+        newGrid[nextBox.leftBoxPosition.row][nextBox.leftBoxPosition.col + 1] =
+          CellType.Empty;
+        newGrid[newBoxRow][newBoxLeftCol] = CellType.LeftBox;
+        newGrid[newBoxRow][newBoxRightCol] = CellType.RightBox;
+      } else {
+        // another box or two, add it to the list to be handled first and then come back to this one
+        if (newGrid[newBoxRow][newBoxLeftCol] !== CellType.Empty) {
+          addBoxToList(newGrid[newBoxRow][newBoxLeftCol], {
+            row: newBoxRow,
+            col: newBoxLeftCol,
+          });
+        }
+
+        if (newGrid[newBoxRow][newBoxRightCol] !== CellType.Empty) {
+          addBoxToList(newGrid[newBoxRow][newBoxRightCol], {
+            row: newBoxRow,
+            col: newBoxRightCol,
+          });
+        }
+
+        // put this box back on the list
+        addBoxToList(CellType.LeftBox, nextBox.leftBoxPosition);
+      }
+    }
+
+    if (isValid) {
+      robot.row = robot.row + move[0];
+      robot.col = robot.col + move[1];
+    }
+  }
+
+  if (isValid) {
+    grid = newGrid;
+  } else {
+    robot.row = previousRobot.row;
+    robot.col = previousRobot.col;
+  }
+  debug && printBoard(grid);
+}
+
+let sum = 0;
+for (let row = 0; row < grid.length; row++) {
+  for (let col = 0; col < grid[0].length; col++) {
+    if (grid[row][col] === CellType.LeftBox) {
+      sum += 100 * row + col;
     }
   }
 }
 
 printBoard(grid);
-
-let sum = 0;
-for (let row = 0; row < grid.length; row++) {
-  for (let col = 0; col < grid[0].length; col++) {
-    // if (grid[row][col] === CellType.Box) {
-    //   sum += 100 * row + col;
-    // }
-  }
-}
-
 console.log({ sum });
